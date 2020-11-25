ad1f3b3fbebb43365d3a74640d424e14b8aca9e0
miaoliming <l>
2020-11-25 16:25:05 +0800

    修复swagger文档问题

--- src/main/java/com/miao/test/pojo/Page.java
+++
@@ -1,9 +0,0 @@
-package com.miao.test.pojo;
-
-import lombok.Data;
-
-@Data
-public class Page {
-     private Integer page;//当前页
-     private Integer pageSize;//每页的记录数
-}
--- users.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `users`(`id`, `name`, `age`, `address`) VALUES (1, '安琪拉', 18, '英雄联盟');
-INSERT INTO `users`(`id`, `name`, `age`, `address`) VALUES (2, '狄仁杰', 16, '王者峡谷');
-INSERT INTO `users`(`id`, `name`, `age`, `address`) VALUES (3, '苗黎明', 18, '深圳');
-INSERT INTO `users`(`id`, `name`, `age`, `address`) VALUES (4, '李易峰', 18, '安徽');
-INSERT INTO `users`(`id`, `name`, `age`, `address`) VALUES (5, '张三', 33, '北京');
-INSERT INTO `users`(`id`, `name`, `age`, `address`) VALUES (6, '李四', 20, '哈尔滨');
